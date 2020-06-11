973f0bbeb074c73e4610094db84832c65e46ed20
Elias Martinez <jemart218@gmail.com>
2020-06-11 13:53:56 -0700

    wireframe commit 2

--- scripts/api.js
+++ scripts/api.js
@@ -1,1 +1,31 @@
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/elias/bookmarks';
+
+/**
+ * listApiFetch - Wrapper function for native `fetch` to standardize error handling. 
+ * @param {string} url 
+ * @param {object} options 
+ * @returns {Promise} 
+ */
+
+const listApiFetch = function (...args) {
+  // setup var in scope outside of promise chain
+  let error;
+  return fetch(...args)
+    .then(res => {
+      if (!res.ok) {
+        error = { code: res.status };
+        if (!res.headers.get('content-type').includes('json')) {
+          error.message = res.statusText;
+          return Promise.reject(error);
+        }
+      }
+      return res.json();
+    })
+    .then(data => {
+      if (error) {
+        error.message = data.message;
+        return Promise.reject(error);
+      }
+      return data;
+    });
+};
